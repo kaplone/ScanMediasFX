b52629550d7cde4d31a44600dc50da37f621136e
kaplone <dvd.brtrnd@gmail.com>
2015-09-25 17:31:13 +0200

    import xlsx + debut parsage

---
+++ ScanMedias_FX/src/application/Main.java
@@ -0,0 +1,39 @@
+package application;
+	
+import java.io.File;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		File file = new File("/home/autor/Telechargements/INVENTAIRE_CHARLY_partiel.xlsx");
+		
+		Documents.write();
+		try {
+			Documents.read(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
